66178c18bec40745e5f444147ac8bfa3474aca91
Arpita Roy <arpita.roy.workar@gmail.com>
2021-08-03 20:15:27 +0530

    The onclick event listene

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 //document.getElementById("count-el").innerText = 5;
 
-// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
-// Decrease it down to 25, and then finally increase it to 70
-// Console.log the value after each step
+// intialize the count as 0
+let count = 0;
+console.log(count);
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+// change the count-el in the HTML to reflect the new count
 
-let bonusPoints = 50;
-console.log(bonusPoints);
- bonusPoints = bonusPoints + 50;
- console.log(bonusPoints);
- bonusPoints = bonusPoints - 75;
- console.log(bonusPoints);
- bonusPoints = bonusPoints + 45;
- console.log(bonusPoints);
+function increment() {
+    console.log('count is zero' );
+}
