80b420173cface06e79bc6fa7c42f577bb804433
Arpita Roy <arpita.roy.workar@gmail.com>
2021-08-03 19:08:04 +0530

    basic mathematical operations

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 //document.getElementById("count-el").innerText = 5;
-let count =0;
-console.log(count);
+
+// 1. Create two variables, myAge and humanDogRatio
+// 2. Multiply the two together and store the result in myDogAge 
+// 3. Log myDogAge to the console
+
 let myAge = 25;
-console.log(myAge);
+let humanDogRatio = 7;
+let myDogAge= myAge * humanDogRatio;
+console.log(myDogAge);
