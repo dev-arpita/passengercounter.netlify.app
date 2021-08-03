99fa741ad5a2b3cd4550da2c3df9775a7e795f2c
Arpita Roy <arpita.roy.workar@gmail.com>
2021-08-03 19:51:26 +0530

    Reassignmenting & incrementing

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 //document.getElementById("count-el").innerText = 5;
 
-// 1. Create two variables, myAge and humanDogRatio
-// 2. Multiply the two together and store the result in myDogAge 
-// 3. Log myDogAge to the console
+// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
+// Decrease it down to 25, and then finally increase it to 70
+// Console.log the value after each step
 
-let myAge = 25;
-let humanDogRatio = 7;
-let myDogAge= myAge * humanDogRatio;
-console.log(myDogAge);
+let bonusPoints = 50;
+console.log(bonusPoints);
+ bonusPoints = bonusPoints + 50;
+ console.log(bonusPoints);
+ bonusPoints = bonusPoints - 75;
+ console.log(bonusPoints);
+ bonusPoints = bonusPoints + 45;
+ console.log(bonusPoints);
