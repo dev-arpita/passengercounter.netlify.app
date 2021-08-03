74282423ca12ce7b2d7d36087add2bdb498f157a
Arpita Roy <arpita.roy.workar@gmail.com>
2021-08-03 20:29:03 +0530

    using function to write less code

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 //document.getElementById("count-el").innerText = 5;
 
 // intialize the count as 0
-let count = 0;
-console.log(count);
+// let count = 0;
+// console.log(count);
 // listen for clicks on the increment button
 // increment the count variable when the button is clicked
 // change the count-el in the HTML to reflect the new count
 
-function increment() {
-    console.log('count is zero' );
+//function increment() {
+//     console.log('count is zero' );
+// }
+function countdown() {
+    console.log(5);
+    console.log(4);
+    console.log(3);
+    console.log(2);
+    console.log(1);
 }
+// Setting up the the race 🏎 🏎 🏎
+
+countdown();
+// GO! 🏁
+// Players are running the race 🏎 💨
+// Race is finished! 🍾
+countdown();
+// Get ready for a new race 🏎 🏎 🏎
