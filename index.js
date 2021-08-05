f81b6695aef1abf64c8fca5b0717c679cc9e5f78
Arpita Roy <arpita.roy.workar@gmail.com>
2021-08-05 20:52:18 +0530

    write a function that logs the sum

--- index.js
+++ index.js
@@ -10,9 +10,12 @@
 //function increment() {
 //     console.log('count is zero' );
 // }
-// Create a function (you decide the name) that logs out the number 42 to the console
-// Call/invoke the function
-function age() {
-    console.log(42);
+let lap1 = 34
+let lap2 = 33
+let lap3 = 36
+
+// Create a function that logs out the sum of all the lap times
+function LapTime() {
+    console.log(lap1 + lap2 + lap3);
 }
-age();
+LapTime();
