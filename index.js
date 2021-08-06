80cb00f9d8ac04441d8225139e097853eb024355
Arpita Roy <arpita.roy.workar@gmail.com>
2021-08-06 19:50:57 +0530

    create a save button

--- index.js
+++ index.js
@@ -13,3 +13,9 @@ function increment() {
 }
 increment();
 // change the count-el in the HTML to reflect the new count
+
+// 1. Create a function, save(), which logs out the count when it's called
+ function save() {
+     console.log(count);
+ }
+ save();
