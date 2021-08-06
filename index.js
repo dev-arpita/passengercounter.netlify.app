36c8e1d27064c16029693880e21c70f7b78c8512
Arpita Roy <arpita.roy.workar@gmail.com>
2021-08-06 20:16:29 +0530

    write your first string variable

--- index.js
+++ index.js
@@ -27,3 +27,6 @@ increment();
 let message = 'you have three new notifications '
 console.log(message);
 console.log(message + username + "!");
+// Create a variable, messageToUser, that contains the message we have logged
+let messageToUser = message + ', ' + username + '!' ;
+console.log(messageToUser);
