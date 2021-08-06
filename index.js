6172589247bf5bc86086f02898d838aa442d4dd7
Arpita Roy <arpita.roy.workar@gmail.com>
2021-08-06 19:33:08 +0530

    display the count

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 //document.getElementById("count-el").innerText = 5;
 
 // intialize the count as 0
-
+let countEl = document.getElementById("count-el");
+console.log(countEl);
 let count = 0;
 
 // listen for clicks on the increment button
 // increment the count variable when the button is clicked
 function increment() {
     count = count + 1;
+    countEl.innerText = count;
 }
+increment();
 // change the count-el in the HTML to reflect the new count
