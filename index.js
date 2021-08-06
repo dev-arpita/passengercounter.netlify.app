889290ec7bea318d26f63f774d75f59dcec749ac
Arpita Roy <arpita.roy.workar@gmail.com>
2021-08-06 19:20:33 +0530

    increment onclicks

--- index.js
+++ index.js
@@ -1,24 +1,12 @@
 //document.getElementById("count-el").innerText = 5;
 
 // intialize the count as 0
-// let count = 0;
-// console.log(count);
+
+let count = 0;
+
 // listen for clicks on the increment button
 // increment the count variable when the button is clicked
-// change the count-el in the HTML to reflect the new count
-
-//function increment() {
-//     console.log('count is zero' );
-// }
-let lapsCompleted = 0
-
-// Create a function that increments the lapsCompleted variable with one
-// Run it three times
-
-function lapsIncrement() {
-    lapsCompleted = lapsCompleted + 1
+function increment() {
+    count = count + 1;
 }
-lapsIncrement();
-lapsIncrement();
-lapsIncrement();
-console.log(lapsCompleted)
+// change the count-el in the HTML to reflect the new count
